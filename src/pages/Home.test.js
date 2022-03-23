6f1717c99168fbc2daf91d311d38086c9202122f
Omar Ragheb <ahmed.raph29@gmail.com>
2022-03-24 00:27:20 +0200

    Test be React library

--- src/pages/Home.test.js
+++ src/pages/Home.test.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { render, screen } from '@testing-library/react';
 import Home from './Home';
 
 it('Home page should render correctly', () => {
   const tree = renderer.create(<Home />).toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+describe('Home Page', () => {
+  test('renders App component', () => {
+    render(<Home />);
+    screen.debug();
+  });
 });
