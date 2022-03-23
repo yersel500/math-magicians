160dcaadf6f71b8ce7cae3e5af4b3a2d035e9ac4
Yersel Hurtado <yersel500@gmail.com>
2022-03-23 14:40:37 -0500

    Test calculate

--- src/tests/calculate.test.js
+++ src/tests/calculate.test.js
@@ -3,7 +3,51 @@ import calculate from "../logic/calculate"
 
 describe( 'Test calculate file', () => {
   test('First calculate test', () => {
-    expect(calculate).toBe()
+
+    const myInput = {
+      total: '2',
+      next: '3',
+      operation: '+',
+    }
+
+    const myOutput = {
+      total: '5',
+      next: null,
+      operation: null,
+    }
+    expect(calculate(myInput, '=')).toEqual(myOutput);
+  })
+
+  test('Second calculate test', () => {
+
+    const myInput = {
+      total: '10',
+      next: '3',
+      operation: '-',
+    }
+
+    const myOutput = {
+      total: '7',
+      next: null,
+      operation: null,
+    }
+    expect(calculate(myInput, '=')).toEqual(myOutput);
+  })
+
+  test('Second calculate test', () => {
+
+    const myInput = {
+      total: '15',
+      next: '5',
+      operation: 'x',
+    }
+
+    const myOutput = {
+      total: '75',
+      next: null,
+      operation: null,
+    }
+    expect(calculate(myInput, '=')).toEqual(myOutput);
   })
 
 })
--- src/tests/operate.test.js
+++ src/tests/operate.test.js
@@ -4,22 +4,22 @@ describe('Test to operator function', () => {
   
   test( 'First test operate', () => {
 
-    expect(operate(9,3,'-')).toBe('6');
+    expect(operate('9','3','-')).toBe('6');
   })
 
   test( 'Second test operate', () => {
 
-    expect(operate(9,3,'+')).toBe('12');
+    expect(operate('9','3','+')).toBe('12');
   })
 
   test( 'Third test operate', () => {
 
-    expect(operate(10,2,'÷')).toBe('5');
+    expect(operate('10','2','÷')).toBe('5');
   })
 
   test( 'Fourth test operate', () => {
 
-    expect(operate(9,3,'x')).toBe('27');
+    expect(operate('9','3','x')).toBe('27');
   })
 
 })
