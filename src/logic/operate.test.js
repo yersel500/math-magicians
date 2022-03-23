c48cf6c60be0069b171fb382b437bd613b945354
Omar Ragheb <ahmed.raph29@gmail.com>
2022-03-23 22:43:01 +0200

    Organizing files

---
+++ src/logic/operate.test.js
@@ -0,0 +1,19 @@
+import operate from './operate';
+
+describe('Test to operator function', () => {
+  test('First test operate', () => {
+    expect(operate('9', '3', '-')).toBe('6');
+  });
+
+  test('Second test operate', () => {
+    expect(operate('9', '3', '+')).toBe('12');
+  });
+
+  test('Third test operate', () => {
+    expect(operate('10', '2', '÷')).toBe('5');
+  });
+
+  test('Fourth test operate', () => {
+    expect(operate('9', '3', 'x')).toBe('27');
+  });
+});
